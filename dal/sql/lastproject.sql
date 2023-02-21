bb80a7adbfd42924bc2cf554e14d061ce1803490
CDsmen <1159230460@qq.com>
2023-02-21 15:41:40 +0800

    test

--- dal/sql/lastproject.sql
+++ dal/sql/lastproject.sql
@@ -135,6 +135,48 @@ LOCK TABLES `video` WRITE;
 INSERT INTO `video` VALUES (1,1,'abab','abab','testvideo');
 /*!40000 ALTER TABLE `video` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'lastproject'
+--
+/*!50003 DROP PROCEDURE IF EXISTS `user_info` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `user_info`(IN user_id int)
+BEGIN
+	declare t_f INT;
+    declare w_c INT;
+    declare f_c INT;
+
+	select COUNT(*) into t_f
+			from video,favorite
+            where video.videoid = favorite.videoid and video.userid = user_id;
+
+	select COUNT(*) into w_c
+			from video
+            where userid = user_id;
+
+	select COUNT(*) into f_c
+			from favorite
+			where userid = user_id;
+
+	select userid id, username name,0 follow_count,0 follower_count, false is_follow,
+			t_f total_favorited, w_c work_count,f_c favorite_count
+            from user where userid = user_id;
+            
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -145,4 +187,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-02-21 15:27:33
+-- Dump completed on 2023-02-21 15:41:18
