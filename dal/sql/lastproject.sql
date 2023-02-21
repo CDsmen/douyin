e9100b1290291e8b062370abac527be8c6afe6cb
CDsmen <1159230460@qq.com>
2023-02-21 15:27:55 +0800

    test

--- dal/sql/lastproject.sql
+++ dal/sql/lastproject.sql
@@ -1,87 +1,148 @@
-/*==============================================================*/
-/* DBMS name:      MySQL 5.0                                    */
-/* Created on:     2023/2/20 17:17:48                           */
-/*==============================================================*/
+-- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+--
+-- Host: localhost    Database: lastproject
+-- ------------------------------------------------------
+-- Server version	8.0.32
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-drop table if exists User;
+--
+-- Table structure for table `comment`
+--
 
-drop table if exists comment;
+DROP TABLE IF EXISTS `comment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `comment` (
+  `commentid` int NOT NULL,
+  `userid` int NOT NULL,
+  `videoid` int NOT NULL,
+  `content` varchar(50) DEFAULT NULL,
+  `create_data` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`commentid`),
+  KEY `FK_user_comment` (`userid`),
+  KEY `FK_vedio_comment` (`videoid`),
+  CONSTRAINT `FK_user_comment` FOREIGN KEY (`userid`) REFERENCES `user` (`userid`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `FK_vedio_comment` FOREIGN KEY (`videoid`) REFERENCES `video` (`videoid`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-drop table if exists favorite;
+--
+-- Dumping data for table `comment`
+--
 
-drop table if exists video;
+LOCK TABLES `comment` WRITE;
+/*!40000 ALTER TABLE `comment` DISABLE KEYS */;
+/*!40000 ALTER TABLE `comment` ENABLE KEYS */;
+UNLOCK TABLES;
 
-/*==============================================================*/
-/* Table: User                                                  */
-/*==============================================================*/
-create table User
-(
-   username             varchar(32),
-   password             varchar(32),
-   userid               int not null,
-   follow_count         int,
-   follower_count       int,
-   avatar               varchar(60),
-   background_image     varchar(60),
-   signature            varchar(60),
-   total_favorited      varchar(60),
-   work_count           int,
-   favorite_count       int,
-   primary key (userid)
-);
+--
+-- Table structure for table `favorite`
+--
 
-/*==============================================================*/
-/* Table: comment                                               */
-/*==============================================================*/
-create table comment
-(
-   commentid            int not null,
-   userid               int not null,
-   videoid              int not null,
-   content              varchar(50),
-   create_data          varchar(20),
-   primary key (commentid)
-);
+DROP TABLE IF EXISTS `favorite`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `favorite` (
+  `likeid` int NOT NULL,
+  `userid` int NOT NULL,
+  `videoid` int NOT NULL,
+  PRIMARY KEY (`likeid`),
+  KEY `FK_user_like` (`userid`),
+  KEY `FK_vedio_like` (`videoid`),
+  CONSTRAINT `FK_user_like` FOREIGN KEY (`userid`) REFERENCES `user` (`userid`) ON DELETE RESTRICT ON UPDATE RESTRICT,
+  CONSTRAINT `FK_vedio_like` FOREIGN KEY (`videoid`) REFERENCES `video` (`videoid`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-/*==============================================================*/
-/* Table: favorite                                              */
-/*==============================================================*/
-create table favorite
-(
-   likeid               int not null,
-   userid               int not null,
-   videoid              int not null,
-   primary key (likeid)
-);
+--
+-- Dumping data for table `favorite`
+--
 
-/*==============================================================*/
-/* Table: video                                                 */
-/*==============================================================*/
-create table video
-(
-   videoid              int not null,
-   userid               int not null,
-   play_url             varchar(60),
-   cover_url            varchar(60),
-   comment_count        int,
-   title                varchar(30),
-   vedio_favorite_count int,
-   primary key (videoid)
-);
+LOCK TABLES `favorite` WRITE;
+/*!40000 ALTER TABLE `favorite` DISABLE KEYS */;
+INSERT INTO `favorite` VALUES (1,1,1);
+/*!40000 ALTER TABLE `favorite` ENABLE KEYS */;
+UNLOCK TABLES;
 
-alter table comment add constraint FK_user_comment foreign key (userid)
-      references User (userid) on delete restrict on update restrict;
+--
+-- Table structure for table `user`
+--
 
-alter table comment add constraint FK_vedio_comment foreign key (videoid)
-      references video (videoid) on delete restrict on update restrict;
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `username` varchar(32) DEFAULT NULL,
+  `password` varchar(32) DEFAULT NULL,
+  `userid` int NOT NULL,
+  `follow_count` int DEFAULT NULL,
+  `follower_count` int DEFAULT NULL,
+  `avatar` varchar(60) DEFAULT NULL,
+  `background_image` varchar(60) DEFAULT NULL,
+  `signature` varchar(60) DEFAULT NULL,
+  `total_favorited` varchar(60) DEFAULT NULL,
+  `work_count` int DEFAULT NULL,
+  `favorite_count` int DEFAULT NULL,
+  PRIMARY KEY (`userid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-alter table favorite add constraint FK_user_like foreign key (userid)
-      references User (userid) on delete restrict on update restrict;
+--
+-- Dumping data for table `user`
+--
 
-alter table favorite add constraint FK_vedio_like foreign key (videoid)
-      references video (videoid) on delete restrict on update restrict;
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES ('testman','1234',1,0,0,'0','0','0','0',0,0);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
 
-alter table video add constraint FK_publish foreign key (userid)
-      references User (userid) on delete restrict on update restrict;
+--
+-- Table structure for table `video`
+--
 
+DROP TABLE IF EXISTS `video`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `video` (
+  `videoid` int NOT NULL,
+  `userid` int NOT NULL,
+  `play_url` varchar(60) DEFAULT NULL,
+  `cover_url` varchar(60) DEFAULT NULL,
+  `title` varchar(30) DEFAULT NULL,
+  PRIMARY KEY (`videoid`),
+  KEY `FK_publish` (`userid`),
+  CONSTRAINT `FK_publish` FOREIGN KEY (`userid`) REFERENCES `user` (`userid`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `video`
+--
+
+LOCK TABLES `video` WRITE;
+/*!40000 ALTER TABLE `video` DISABLE KEYS */;
+INSERT INTO `video` VALUES (1,1,'abab','abab','testvideo');
+/*!40000 ALTER TABLE `video` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-02-21 15:27:33
